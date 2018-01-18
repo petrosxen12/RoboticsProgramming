65148091e0de3db92fce1512324af22f6d4db692
Petros Xen <petros.x12@gmail.com>
2018-01-18 01:05:49 +0000

    attempted to make a square. failed.

--- testing.c
+++ testing.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 #ifdef _WIN32
 //add definition of struct timeval in Windows
 #include <winsock.h>
@@ -11,32 +12,47 @@
 #include "simpletools.h"
 #include "ping.h"
 
-static void showTicks(int calibration)
-{
-  int left, right;
-  drive_getTicks(&left, &right);
-  print("%d %d\n", left+calibration, right+calibration);
+#ifdef BUILDING_IN_SIMULATOR
+#include "simulator.h"
+#endif
+
+static void showTicks(int calibration){
+	int left, right;
+	drive_getTicks(&left, &right);
+	print("%d %d\n", left+calibration, right+calibration);
 }
 
 int metersToTicks(int meters){
-  int milimeters = meters*1000;
-  int mmToTicks = milimeters/3.25;
-  return mmToTicks;
+	int milimeters = meters*1000;
+	int mmToTicks = milimeters/3.25;
+	return mmToTicks;
 }
 
-int main(int argc, const char* argv[])
-{	
+int main(int argc, const char* argv[]){	
 	int offsetVal = -120;
-	
 	int ticksToMove = metersToTicks(1);
 
-	drive_speed(-1,1);
-	drive_goto(offsetVal,offsetVal); //Offset 
-   	drive_goto(ticksToMove,ticksToMove); //Drive one meter 
+	#ifdef BUILDING_IN_SIMULATOR
+	//simulator_startNewSmokeTrail();
+	#endif
 
-   //Turn 90 Degrees
-   drive_goto(51,0);
-   showTicks(abs(offsetVal));
+	drive_goto(offsetVal,offsetVal); //Initial offset so robot can have space
+	drive_speed(1,1);
+	drive_goto(20,20);
+	//drive_goto(-51,0);
+	//drive_goto(102,0);
+	
+	for (int i = 0; i < 4; ++i){
+	drive_speed(1,1);
+	drive_goto(ticksToMove,ticksToMove); //Drive one meter 
+	drive_goto(-51,0);	
+	}
+
+		#ifdef BUILDING_IN_SIMULATOR
+		//simulator_stopNewSmokeTrail();
+	#endif
+
+	showTicks(abs(offsetVal));
 }
 
 
