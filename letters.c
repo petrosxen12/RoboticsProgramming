4c62af9fef2dcb0461ca06c9e09c840cbbcdd495
Petros Xen <petros.x12@gmail.com>
2018-01-19 14:17:31 +0000

    Added letter functions.

--- letters.c
+++ letters.c
@@ -16,27 +16,178 @@
 #include "simulator.h"
 #endif
 
+void makeTurnLeftNR()
+{
+		drive_goto(-8,-8);
+		simulator_stopSmokeTrail();
+		drive_goto(-26,25);
+		drive_goto(8,8);
+}
+
+void makeTurnRightNR()
+{
+  drive_goto(-10,-10);
+  simulator_stopSmokeTrail();
+  drive_goto(26,-25);
+  drive_goto(8,8);
+}
+
 void drawA(){
-	print("A");
+  drive_goto(-20,20);
+  simulator_startNewSmokeTrail();
+  drive_goto(82,82);
+  simulator_stopSmokeTrail();
+  drive_goto(-23,0);
+  drive_goto(12,12);
+  simulator_startNewSmokeTrail();
+  drive_goto(-80,-80);
+  simulator_stopSmokeTrail();
+  drive_goto(30,30);
+  drive_goto(-20,20);
+  drive_goto(8,8);
+  simulator_startNewSmokeTrail();
+  drive_goto(26,26);
+  simulator_stopSmokeTrail();
+
+  // reset starting position
+  drive_goto(-33,-33);
+  drive_goto(-28,25);
+  drive_goto(30,30);
+  drive_goto(-26,25);
+  drive_goto(30,30);
+
 }
+
 void drawE(){
-	print("E");
-	//print("hi");
+  simulator_startNewSmokeTrail();
+  drive_goto(40,40);
+  simulator_stopSmokeTrail();
+  drive_goto(-40,-40);
+  makeTurnLeftNR();
+  simulator_startNewSmokeTrail();
+  drive_goto(40,40);
+  makeTurnRightNR();
+  simulator_startNewSmokeTrail();
+  drive_goto(40,40);
+  simulator_stopSmokeTrail();
+  drive_goto(-40,-40);
+  makeTurnLeftNR();
+  simulator_startNewSmokeTrail();
+  drive_goto(30,30);
+  makeTurnRightNR();
+  simulator_startNewSmokeTrail();
+  drive_goto(40,40);
+  simulator_stopSmokeTrail();
+
+   // reset starting position
+   drive_goto(25,-25);
+   drive_goto(70,70);
+   drive_goto(-25,25);
+   drive_goto(10,10);
 }
-void drawF (){
-	print("F");
+
+void drawF(){
+  drive_goto(-8,-8);
+  drive_goto(-26,25);
+  drive_goto(8,8);
+  simulator_startNewSmokeTrail();
+  drive_goto(40,40);
+  makeTurnRightNR();
+  drive_goto(40,40);
+  simulator_startNewSmokeTrail();
+  drive_goto(-40,-40);
+  simulator_stopSmokeTrail();
+  makeTurnLeftNR();
+  simulator_startNewSmokeTrail();
+  drive_goto(30,30);
+  makeTurnRightNR();
+  simulator_startNewSmokeTrail();
+  drive_goto(40,40);
+  simulator_stopSmokeTrail();
+
+  // reset starting position
+  drive_goto(-25,25);
+  drive_goto(-70,-70);
+  drive_goto(25,-25);
+  drive_goto(20,20);
 }
+
 void drawH(){
-	print("H");
+
+  drive_goto(-8,-8);
+  drive_goto(-26,25);
+  drive_goto(8,8);
+  simulator_startNewSmokeTrail();
+  drive_goto(80,80);
+  simulator_stopSmokeTrail();
+  drive_goto(-32,-32);
+  drive_goto(-10,-10);
+  drive_goto(26,-25);
+  drive_goto(8,8);
+  simulator_startNewSmokeTrail();
+  drive_goto(40,40);
+  makeTurnLeftNR();
+  drive_goto(32,32);
+  simulator_startNewSmokeTrail();
+  drive_goto(-80,-80);
+  simulator_stopSmokeTrail();
+
+  //reset starting position
+  drive_goto(25,-25);
+  drive_goto(20,20);
+
 }
+
 void drawI(){
-	print("I");
+  drive_goto(-8,-8);
+  drive_goto(-26,25);
+  drive_goto(8,8);
+  drive_goto(80,80);
+  simulator_startNewSmokeTrail();
+  drive_goto(-80,-80);
+  makeTurnRightNR();
+  drive_goto(20,20);
 }
-void drawK(){
-	print("K");
-}
+
 void drawL(){
-	print("L");
+	simulator_startNewSmokeTrail();
+  drive_goto(40,40);
+	simulator_stopSmokeTrail();
+  drive_goto(-40,-40);
+	drive_goto(-8,-8);
+	drive_goto(-26,25);
+	drive_goto(8,8);
+	simulator_startNewSmokeTrail();
+  drive_goto(70,70);
+  simulator_stopSmokeTrail();
+
+  //reset starting position
+  drive_goto(60,60);
+}
+
+void drawK(){
+  drive_goto(-8,-8);
+  drive_goto(-26,25);
+  drive_goto(8,8);
+  simulator_startNewSmokeTrail();
+  drive_goto(70,70);
+  simulator_stopSmokeTrail();
+  drive_goto(-35,-35);
+  drive_goto(13,-13);
+  drive_goto(8,8);
+  drive_goto(50,50);
+  simulator_startNewSmokeTrail();
+  drive_goto(-50,-50);
+  makeTurnRightNR();
+  simulator_startNewSmokeTrail();
+  drive_goto(50,50);
+  simulator_stopSmokeTrail();
+  drive_goto(-8,-8);
+  drive_goto(-13,13);
+  drive_goto(8,8);
+
+  //reset starting position
+  drive_goto(20,20);
 }
 
 
