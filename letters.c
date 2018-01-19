a21456953df99f4d1385313f4974bf68347acbcf
Petros Xen <petros.x12@gmail.com>
2018-01-19 00:19:38 +0000

    Added input function in letters.c

--- letters.c
+++ letters.c
@@ -15,7 +15,72 @@
 #ifdef BUILDING_IN_SIMULATOR
 #include "simulator.h"
 #endif
+void drawA(){
+	printf("A");
+}
+void drawE(){
+	printf("E");
+}
+void drawF (){
+	printf("F");
+}
+void drawH(){
+	printf("H");
+}
+void drawI(){
+	printf("I");
+}
+void drawK(){
+	printf("K");
+}
+void drawL(){
+	printf("L");
+}
+
+
+char inputLetters[8];
 
 int main(int argc, const char* argv[]){	
+
+	for (int i = 0; i < 8; ++i)
+	{
+		inputLetters[i] = 'n';
+	}
+
+	print("Enter letters (maximum 7):\n");
+	getStr(inputLetters,7);
+	int j=0;
+	while(inputLetters[j] != 'n'){
+		switch(inputLetters[j]){
+			case 'A':
+				drawA();
+			break;
+
+			case 'E':
+				drawE();
+			break;
+
+			case 'F':
+				drawF();
+			break;
+
+			case 'H':
+				drawH();
+			break;
+
+			case 'I':
+				drawI();
+			break;
+			
+			case 'K':
+				drawK();
+			break;
+
+			case 'L':
+				drawL();
+			break;
+	}
+			j++;
+	}
 
 }
